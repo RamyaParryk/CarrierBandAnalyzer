a1cf46a1482ee51e5e34a808e9404041ad92fd0e
RamyaParryk <ramyaparryk@gmail.com>
2026-03-30 03:11:05 +0900

    Remove app/src from GitHub

--- app/src/main/java/com/ratolab/carrierbandanalyzer/CaChannelDebug.kt
+++
@@ -1,8 +0,0 @@
-package com.ratolab.carrierbandanalyzer
-
-data class CaChannelDebug(
-    val networkType: Int,
-    val networkTypeName: String,
-    val channelNumber: Int,
-    val bandDirect: Int?
-)
